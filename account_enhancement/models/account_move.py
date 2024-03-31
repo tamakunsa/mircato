662b6068230acde88cc40eae79035273ef93aeb9
Omnya Rashwan <o.reshwan@tatayab.com>
2024-03-31 02:32:08 +0200

    [FIX] account_enhancement: FIx error in journal entry

--- account_enhancement/models/account_move.py
+++ account_enhancement/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import fields, models, api, _
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    change_analytic_distribution = fields.Boolean(compute='check_confirm_bill',
+    change_analytic_distribution = fields.Boolean(compute='check_change_analytic_distribution',
                                                   default=lambda self: self.env.user.has_group(
                                                       'account_enhancement.group_control_entry_analytic_account'))
 

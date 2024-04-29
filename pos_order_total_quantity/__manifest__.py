0d06e6217749346ee459921b227a216062ce0b01
khadrawy <elkhadrawy222@gmail.com>
2024-04-29 21:35:58 +0300

    [ADD] pos_order_total_quantity 3rd_party app

---
+++ pos_order_total_quantity/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Pos Order Total Quantity",
+    "author": "IdeaCode Academy",
+    "depends": ["point_of_sale"],
+    "images":["static/description/img.png"],
+    "assets": {
+        'point_of_sale._assets_pos': [
+            "pos_order_total_quantity/static/src/**/*"
+        ]
+    },
+    "license": "LGPL-3",
+"price": "0",
+    "currency": "USD",
+}

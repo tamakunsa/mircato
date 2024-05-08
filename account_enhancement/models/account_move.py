d43bb5ff41546c7b29d05a4574e239e74e78b377
Omnya Rashwan <o.reshwan@tatayab.com>
2024-05-08 23:35:54 +0300

    [MOD] account_enhancement: Modify on journal type in journal entry

--- account_enhancement/models/account_move.py
+++ account_enhancement/models/account_move.py
@@ -14,3 +14,19 @@ class AccountMove(models.Model):
             if self.env.user.has_group(
                     'account_enhancement.group_control_entry_analytic_account'):
                 rec.change_analytic_distribution = True
+
+    @api.depends('company_id', 'invoice_filter_type_domain')
+    def _compute_suitable_journal_ids(self):
+        for m in self:
+            journal_type = m.invoice_filter_type_domain or 'general'
+            company = m.company_id or self.env.company
+            m.suitable_journal_ids = self.env['account.journal'].search([
+                *self.env['account.journal']._check_company_domain(company),
+                ('type', '=', journal_type),
+            ])
+            if m.move_type == 'entry':
+                journal_types = 'sale', 'purchase', 'bank', 'cash', 'general'
+                m.suitable_journal_ids = self.env['account.journal'].search([
+                    *self.env['account.journal']._check_company_domain(company),
+                    ('type', 'in', journal_types),
+                ])

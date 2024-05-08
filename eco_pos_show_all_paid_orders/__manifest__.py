6e06e696997cab63f4d0cf1d41dad02a2e40eaa1
omarBnZaky <omarbnzaky@gmail.com>
2024-05-08 16:53:36 +0300

    [ADD]eco_pos_show_all_paid_orders

---
+++ eco_pos_show_all_paid_orders/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "eco show all Pos paid orders",
+
+    'summary': "Show All Pos orders in refund button",
+
+    'description': """
+Show All Pos orders in refund button
+    """,
+
+    'author': "Eco-Tech, Omar Amr",
+    'website': "https://ecotech.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+
+    'category': 'Products',
+    'version': '17.0.1.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'point_of_sale'
+    ],
+
+
+}
+
